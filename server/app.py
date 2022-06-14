727765f638ac760e57f0fb7ad2751de2279b127c
nazhimkalam <nazhimkalam@gmail.com>
2022-06-14 11:46:01 +0530

    server running properly

---
+++ server/app.py
@@ -0,0 +1,44 @@
+from distutils.log import debug
+import joblib
+import numpy as np
+import pandas as pd
+import re
+
+from flask import Flask
+from flask_cors import CORS
+from flask_restful import Api, Resource, reqparse
+
+APP = Flask(__name__)
+APP.config['CORS_HEADERS'] = 'Content-Type'
+CORS(APP)
+API = Api(APP)
+
+
+model = joblib.load('model')
+
+class Predict(Resource):
+
+    def clean_text(text):
+        text = text.lower()
+        text = re.sub(r"(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", "", text)
+        return text
+
+    @staticmethod
+    def post():
+        parser = reqparse.RequestParser()
+        parser.add_argument('review')
+        
+        requestBody = parser.parse_args()  # creates dictionary
+
+        categories = ["Negative", "Positive"]
+        result = int(model.predict(pd.Series(requestBody.review))[0])
+
+        out = {'Prediction': categories[result]}
+
+        return out, 200
+
+
+API.add_resource(Predict, '/predict')
+
+if __name__ == '__main__':
+    APP.run(port=5003, debug=True)
