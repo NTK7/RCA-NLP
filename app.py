829fedc1bde98435a8147212b293db6e1b74d543
nazhimkalam <nazhimkalam@gmail.com>
2022-06-26 12:21:50 +0530

    created the sentense review checks

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from unittest import result
 import joblib
 import pandas as pd
 import re
@@ -22,26 +23,50 @@ class Predict(Resource):
         text = re.sub(r"(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", "", text)
         return text
 
-    def getReviewKeyWords(text):
+    # def getReviewKeyWords(text):
+    #     words = text.split()
+    #     words = [word.lower() for word in words]
+    #     words = [word.strip('\'"?,.') for word in words]
+
+    #     stop_words = get_stop_words('en')
+    #     meaningful_words = [w for w in words if not w in stop_words]
+
+    #     categories = ["Negative", "Positive"]
+    #     review = int(model.predict(pd.Series(text))[0])
+    #     reviewKeyWordResult = []
+
+    #     for word in meaningful_words:
+    #         keywordReview = int(model.predict(pd.Series(word))[0])
+    #         reviewKeyWordResult.append({"keyword": word, "review": categories[keywordReview]})
+        
+    #     return {"prediction": categories[review], "keywords": reviewKeyWordResult}
+
+    def getReviewKeyWords(review):
+        sentenses = review.split(".")
+        sentenses = [sentense.lower() for sentense in sentenses]
+        sentenses = [sentense.strip('\'"?,.') for sentense in sentenses]
+
         stop_words = get_stop_words('en')
-        text = Predict.clean_text(text)
-        text = text.split()
-        text = [word for word in text if word not in stop_words]
-        return text
+        meaningful_words = [w for w in sentenses if not w in stop_words]
+
+        categories = ["Negative", "Positive"]
+        review_result = int(model.predict(pd.Series(review))[0])
+        reviewSentenseResult = []
+
+        for sentense in meaningful_words:
+            sentense_result = int(model.predict(pd.Series(sentense))[0])
+            reviewSentenseResult.append({"sentense": sentense, "review": categories[sentense_result]})
+
+        return {"prediction": categories[review_result], "sentenses": reviewSentenseResult}
 
     @staticmethod
     def post():
         parser = reqparse.RequestParser()
         parser.add_argument('review')
         
-        requestBody = parser.parse_args()  # creates dictionary
-
-        categories = ["Negative", "Positive"]
-        result = int(model.predict(pd.Series(requestBody.review))[0])
-
-        keyWords = Predict.getReviewKeyWords(requestBody.review)
-        print("keyWords", str(keyWords))
-        out = {'Prediction': categories[result], "KeyWords": keyWords}
+        requestBody = parser.parse_args() 
+        result = Predict.getReviewKeyWords(requestBody.review)
+        out = {'Prediction': result['prediction'], "Keys": result['sentenses']}
 
         return out, 200
 
